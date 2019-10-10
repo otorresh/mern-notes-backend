f49d3b2031a00ae51f17024dd9dc0640ecc753cc
otorresh <otorresh@live.com>
2019-10-09 23:58:46 -0500

    Primera version del backend

---
+++ src/controllers/notes.controllers.js
@@ -0,0 +1,44 @@
+const notesCtrl = {};
+
+const Note = require('../models/Note');
+
+notesCtrl.getNotes = async (req, res) => {
+    const notes = await Note.find();
+    res.json(notes);
+}
+
+notesCtrl.createNote = async (req, res) => {
+    const { title, content, date, author } = req.body;
+    const newNote = new Note({
+        title,
+        content,
+        date,
+        author
+    });
+    await newNote.save();
+    res.json({ message: "Note Saved" });
+};
+
+notesCtrl.getNote = async (req, res) => {
+    const note = await Note.findById(req.params.id);
+    console.log(note);
+    res.json(note);
+};
+
+notesCtrl.updateNote = async (req, res) => {
+    const { title, content, date, author } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {
+        title,
+        content,
+        date,
+        author
+    });
+    res.json({ message: "Note Updated" });
+}
+
+notesCtrl.deleteNote = async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    res.json({ message: "Note Deleted" });
+}
+
+module.exports = notesCtrl;
